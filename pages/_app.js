07c31539ed315d9f07ed89bef6189365a25324e1
PEREZ CASTRO PHILIP JUNIOR <jperez.c92@gmail.com>
2020-08-24 16:29:19 -0500

    deploy to vercel

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/global.scss';
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <Component {...pageProps} />
+  );
+};
+
+export default App;
